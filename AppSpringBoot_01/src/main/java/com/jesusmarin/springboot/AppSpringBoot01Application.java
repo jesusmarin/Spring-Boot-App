5c519ce0d8a8aedbe7c60c07d961a16ec8a7e0c6
JesusMarin <JesusMarin@DESKTOP-FNF04LI>
2019-09-17 12:51:58 -0500

    Commit inicial con eclipse

---
+++ AppSpringBoot_01/src/main/java/com/jesusmarin/springboot/AppSpringBoot01Application.java
@@ -0,0 +1,13 @@
+package com.jesusmarin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBoot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringBoot01Application.class, args);
+	}
+
+}
